34bf8b4e666c78f7740dd829dbefea11f9b77ed4
Irakli Chkuaseli <i@irak.li>
2016-11-20 14:32:19 +0400

    Add read input

--- Termites/Termites.cpp
+++ Termites/Termites.cpp
@@ -1,7 +1,26 @@
+#include <iostream>
+#include <vector>
 #include <stdio.h>
 
+using namespace std;
+
+int termites(vector<int> &input) 
+{
+	int value = 0;
+	 
+}
 
 int main()
 {
+	int x = 0;
+	vector<int> input;
+	cin >> x;
+	for (int i = 0; i < x; i++)
+	{
+		int n;
+		cin >> n;
+		input.push_back(n);
+	}
 
+	cin >> x;
 }
